104effe2a2ffffbe3d2e12cbbff99ffabfd4028f
GreatGob <anhlefd11@gmail.com>
2022-07-20 16:50:04 +0700

    update

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -34,13 +34,13 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'rest_framework',
     'snippets',
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions',
 ]
 
 MIDDLEWARE = [
